f89a7e1d2273f546875770c938ec464201992e26
Mateo <mateom.realtor@gmail.com>
2022-09-09 11:20:46 -0400

    created seeds, models & routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,3 +3,5 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -33,11 +33,7 @@ app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: true, limit: "50mb"}));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(function (req, res, next) {
-    routes
-    next();
- });
- 
+app.use(require('./controllers/'));
 // app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
