73e2936dcdff50ae69187ac85db0a92ed6c991ca
Mateo <mateom.realtor@gmail.com>
2022-09-11 21:36:16 -0400

    updated the comment funtionality

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,16 +1,22 @@
 module.exports = {
-    format_time: (date) => {
-      return date.toLocaleTimeString();
-    },
-    format_date: (date) => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-        new Date(date).getFullYear() + 5
-      }`;
-    },
-    format_url: (url) => {
-      return url;
-    },
-    format_plural: (attribute, val) => {
-      return attribute + " " + val + " times";
+  format_date: date => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+      date
+    ).getFullYear()}`;
+  },
+  format_url: url => {
+    return url
+      .replace('http://', '')
+      .replace('https://', '')
+      .replace('www.', '')
+      .split('/')[0]
+      .split('?')[0];
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
     }
-  };
+
+    return word;
+  }
+};
